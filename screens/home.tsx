a6da00c3e86950ccc4ce11aa59c9e2f52774a4da
ilci66 <akbiyik.ilker7@gmail.com>
2021-12-22 21:50:11 +0100

    rendering images

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,111 +1,113 @@
-import  React, { useEffect } from 'react';
-import { StyleSheet, TouchableOpacity, Appearance, ScrollView } from 'react-native';
+import  React, { useEffect, useState } from 'react';
+import { StyleSheet, TouchableOpacity, Appearance, ScrollView, Image, FlatList } from 'react-native';
 
 import { Card, Button, Icon } from 'react-native-elements'
 import customBtn from '../constants/CustomStyles';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
-import { RootTabScreenProps } from '../types';
+import { RootTabScreenProps, Drawings } from '../types';
 
 export default function Home({ navigation }: RootTabScreenProps<'Home'>) {
 
   const colorScheme = Appearance.getColorScheme();
 
+  const [ loading, setLoading ] = useState<boolean | null>(true)
+  
+  const [ drawingData, setDrawingData ] = useState<Drawings>(undefined)
+
   const updateDrawing = () => {
     console.log('I wanna update the drawing')
   }
 
+  const mapDrawingsToCards = () => {
+    if(setDrawingData !== undefined) {
+      return <ScrollView 
+      // fixed the very slow scrolling 
+      removeClippedSubviews={true}>  
+      {drawingData!.map((drawing, i) => {
+        return(
+          <Card key={i}>
+          <Card.Title>{drawing.title}</Card.Title>
+          <Card.Divider />
+          <Card.Image
+            style={{ padding: 0 }}
+            source={{ uri: `${drawing.url}.jpg` }}
+          />
+          <Text style={{ marginBottom: 10 }}>
+            {drawing.description}
+          </Text>
+          <TouchableOpacity 
+          onPress={getDrawings} 
+          style={colorScheme == 'dark' ? customBtn.btnDark: customBtn.btnLight}
+        >
+          <Text 
+            style={colorScheme == 'dark' ? customBtn.btnTextDark: customBtn.btnTextLight}>
+              Alter Object Types
+          </Text>
+        </TouchableOpacity>
+          {/* <Button
+            icon={
+              <Icon
+                name="code"
+                color="#ffffff"
+                iconStyle={{ marginRight: 10 }}
+              />
+            }
+            buttonStyle={{
+              borderRadius: 0,
+              marginLeft: 0,
+              marginRight: 0,
+              marginBottom: 0,
+            }}
+            title="VIEW NOW"
+          /> */}
+          </Card>
+        )
+      })}</ScrollView>
+    }
+  }
+
   const getDrawings = () => {
-    return fetch('192.168.1.4:3002/drawings')
+    setLoading(true)
+    fetch('http://192.168.1.5:3002/drawings')
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(async data => {
+        await setDrawingData(data)
+        await setLoading(false)
+        console.log("just to make sure it's getting data ==>",data[0].title)
+        console.log('got all the drawings')
+      })
   }
  
   useEffect(() => {
-    console.log('fetch data when the app loads')
-  //   fetch('192.168.1.4:3002/drawings')
-  //     .then(res => res.json())
-  //     .then(data => console.log(data))
+    getDrawings()
   }, [])
 
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Home</Text>
-      <Text>Drawings will be displayed as cards with the option toupdate the object information</Text>
-      <Text>Might add a filter option at the top of the page for object types, not sure yet</Text>
+      <Text>
+        Drawings will be displayed as cards with the option toupdate the object information
+      </Text>
+      <Text>
+        Might add a filter option at the top of the page for object types, not sure yet
+      </Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <TouchableOpacity onPress={updateDrawing} style={colorScheme == 'dark' ? customBtn.btnDark: customBtn.btnLight}>
-        <Text style={colorScheme == 'dark' ? customBtn.btnTextDark: customBtn.btnTextLight}>asdasd </Text>
+      <TouchableOpacity 
+        onPress={getDrawings} 
+        style={colorScheme == 'dark' ? customBtn.btnDark: customBtn.btnLight}
+      >
+        <Text 
+          style={colorScheme == 'dark' ? customBtn.btnTextDark: customBtn.btnTextLight}>
+            asdasd 
+        </Text>
       </TouchableOpacity>
-      {/* <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
-      <ScrollView>
-        
-      <Card>
-            <Card.Title>HELLO WORLD</Card.Title>
-            <Card.Divider />
-            <Card.Image
-              style={{ padding: 0 }}
-              source={{
-                uri:
-                  'https://awildgeographer.files.wordpress.com/2015/02/john_muir_glacier.jpg',
-              }}
-            />
-            <Text style={{ marginBottom: 10 }}>
-              The idea with React Native Elements is more about component
-              structure than actual design.
-            </Text>
-            <Button
-              icon={
-                <Icon
-                  name="code"
-                  color="#ffffff"
-                  iconStyle={{ marginRight: 10 }}
-                />
-              }
-              buttonStyle={{
-                borderRadius: 0,
-                marginLeft: 0,
-                marginRight: 0,
-                marginBottom: 0,
-              }}
-              title="VIEW NOW"
-            />
-          </Card>
-          
-      <Card>
-            <Card.Title>HELLO WORLD</Card.Title>
-            <Card.Divider />
-            <Card.Image
-              style={{ padding: 0 }}
-              source={{
-                uri:
-                'https://imgur.com/gLFiC4o',
-              }}
-            />
-            <Text style={{ marginBottom: 10 }}>
-              The idea with React Native Elements is more about component
-              structure than actual design.
-            </Text>
-            <Button
-              icon={
-                <Icon
-                  name="code"
-                  color="#ffffff"
-                  iconStyle={{ marginRight: 10 }}
-                />
-              }
-              buttonStyle={{
-                borderRadius: 0,
-                marginLeft: 0,
-                marginRight: 0,
-                marginBottom: 0,
-              }}
-              title="VIEW NOW"
-            />
-          </Card>
-      </ScrollView>
+      {loading && <Text>loading</Text>}
+      {loading == false && mapDrawingsToCards()}
+      <Text>sdasd</Text>
+      {/* {drawingData!==undefined ? mapDrawingsToCards() : null} */}
     </View>
   );
 }
@@ -125,5 +127,9 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
-
+  image: {
+    width: 30,
+    height: 30,
+    marginRight: 10,
+  },
 });
--- types.tsx
+++ types.tsx
@@ -19,6 +19,12 @@ export type RootStackParamList = {
   NotFound: undefined;
 };
 
+export type Drawings = {
+  url:string;
+  title: string;
+  description: string;
+}[] | undefined;
+
 export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
   RootStackParamList,
   Screen
