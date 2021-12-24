1cb4667d91e97c8fa824ac3174f17cd3a0736c61
ilci66 <akbiyik.ilker7@gmail.com>
2021-12-24 11:12:06 +0100

    cooler icons

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -67,7 +67,7 @@ function BottomTabNavigator() {
         component={HomeScreen}
         options={({ navigation }: RootTabScreenProps<'Home'>) => ({
           title: 'Home',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
           headerRight: () => (
             <Pressable
               onPress={() => navigation.navigate('Modal')}
@@ -89,7 +89,7 @@ function BottomTabNavigator() {
         component={TabTwoScreen}
         options={{
           title: 'Carousel',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="grav" color={color} />,
         }}
       />
     </BottomTab.Navigator>
